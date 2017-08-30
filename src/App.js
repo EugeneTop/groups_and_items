b2c9df6a101c064235f83c94e15a0e7d25661e4b
EugeneTop <moonkin973@gmail.com>
2017-08-30 16:50:06 +0300

    comm

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import { getGroups } from './action/groups';
 import { getItems } from "./action/items";
 import { getCount } from "./action/count";
 import { getInfoItem } from "./action/infoItem";
-import groups from "./reducers/groups";
 
 class App extends Component {
 
     componentWillMount() {
         this.props.onGetGroups();
-        this.props.onGetItems('d3956052-8110-11e7-bb31-be2e44b06b34');
+
+        if(localStorage.getItem('myDate') === null){
+            localStorage.setItem('myDate', 'd3956052-8110-11e7-bb31-be2e44b06b34');
+        }
+
+        this.props.onGetItems(localStorage.getItem('myDate'));
+
         setTimeout(() => {
             this.props.groups.map((group) => {
                 this.props.onGetCount(group.id);
@@ -27,54 +32,57 @@ class App extends Component {
         }, 2000);
     }
 
-  render() {
-    return (
-        <div className="row">
-          <div className="App1">
-            <div className="col-md-1">
-              <img id="img1" src={logo} alt=""/>
-            </div>
-            <div className="col-md-4 text-left">
-              <h4 className="list">Item collection list</h4>
-            </div>
-          </div>
-          <div className="col-md-3 body">
-                {
-                  this.props.groups.map((group, index) =>
-                    <p key={index}><span className="pointer" onClick={ this.props.onGetItems.bind(this, group.id) }>{group.name}</span><input className="inp" id='count' ref={index}/></p>
-                )}
-          </div>
-          <div className="second col-md-9">
-                  {
-                  this.props.items.map((item, index) =>
-                      <div className="col-md-12 title" key={index}><span className="itemTitle col-md-3  text-center" data-toggle="modal" data-target="#myModalBox" onClick={this.props.onGetInfoItems.bind(this, item.id)}>{item.title}</span><span className="col-md-4  text-center">{item.data}</span><span className="col-md-3  text-center">{item.subtitle}</span></div>
-                  )}
-          </div>
-            <div id="myModalBox" className="modal fade">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-body">
-                            <div className="col-md-7">
-                                <img id="img" src={pg} alt=""/>
-                            </div>
-                            <div className="col-md-5">
-                                <p className="p">{this.props.infoItem.name}</p>
-                                <p className="p">{this.props.infoItem.subtitle}</p>
-                                <p className="p">{this.props.infoItem.date}</p>
-                            </div>
-                            <div className="col-md-12">
-                                <p>{this.props.infoItem.description}</p>
-                            </div>
-                            <div className="col-md-12 text-center">
-                                <button id="btns" data-dismiss="modal" aria-hidden="true">Close</button>
+    render() {
+        return (
+            <div className="row">
+                <div className="App1">
+                    <div className="col-md-1">
+                        <img id="img1" src={logo} alt=""/>
+                    </div>
+                    <div className="col-md-4 text-left">
+                        <h4 className="list">Item collection list</h4>
+                    </div>
+                </div>
+                <div className="col-md-3 body">
+                    {
+                        this.props.groups.map((group, index) =>
+                            <p key={index}><span className="pointer" onClick={() => {
+                                this.props.onGetItems(group.id);
+                                localStorage.setItem('myDate', group.id);
+                            } }>{group.name}</span><input className="inp" id='count' ref={index}/></p>
+                        )}
+                </div>
+                <div className="second col-md-9">
+                    {
+                        this.props.items.map((item, index) =>
+                            <div className="col-md-12 title" key={index}><span className="itemTitle col-md-3  text-center" data-toggle="modal" data-target="#myModalBox" onClick={this.props.onGetInfoItems.bind(this, item.id)}>{item.title}</span><span className="col-md-4  text-center">{item.data}</span><span className="col-md-3  text-center">{item.subtitle}</span></div>
+                        )}
+                </div>
+                <div id="myModalBox" className="modal fade">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-body">
+                                <div className="col-md-7">
+                                    <img id="img" src={pg} alt=""/>
+                                </div>
+                                <div className="col-md-5">
+                                    <p className="p">{this.props.infoItem.name}</p>
+                                    <p className="p">{this.props.infoItem.subtitle}</p>
+                                    <p className="p">{this.props.infoItem.date}</p>
+                                </div>
+                                <div className="col-md-12">
+                                    <p>{this.props.infoItem.description}</p>
+                                </div>
+                                <div className="col-md-12 text-center">
+                                    <button id="btns" data-dismiss="modal" aria-hidden="true">Close</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
 
 export default connect(
@@ -89,8 +97,8 @@ export default connect(
             dispatch(getGroups());
         },
         onGetItems: (idI) => {
-          const itemId = idI;
-              dispatch(getItems(itemId));
+            const itemId = idI;
+            dispatch(getItems(itemId));
         },
         onGetCount: (Iid) => {
             const itemId = Iid;
